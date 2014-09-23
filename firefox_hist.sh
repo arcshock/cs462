40356f39bf51ba8cc6d2d507579f48667a2bce22
Bucky Frost <frozenouthere@gmail.com>
2014-09-22 23:43:49 -0800

    Modified firefox history script.
    Attempted to clean up the script a bit. Still requires attention.

--- firefox_hist.sh
+++ firefox_hist.sh
@@ -16,15 +16,14 @@ main() {
 
 	for DIR in $HOME_DIRS
 	do
-		PROFILE=${DIR}"/.mozilla/firefox/profiles.ini"
-		FIREFOX_PROFILE=${DIR}"/.mozilla/firefox/"	
-		if [ -f $PROFILE ]
+		FIREFOX_DIR=${DIR}"/.mozilla/firefox/"	
+		FIREFOX_PROFILE=${FIREFOX_DIR}"/profiles.ini"
+		if [ -f $FIREFOX_PROFILE ]
 		then
-			cd $FIREFOX_PROFILE$(grep -i path $PROFILE | awk -F= '{print $2}')
-			TEST=$FIREFOX_PROFILE$(grep -i path $PROFILE | awk -F= '{print $2}')
-			echo $TEST'/places.sqlite'
-			#echo $(sqlite3 $TEST'places.sqlite' $DB_QUERY)
-			echo "$(sqlite3 $TEST'/places.sqlite' 'SELECT datetime(moz_historyvisits.visit_date/1000000,"unixepoch"), moz_places.url FROM moz_places, moz_historyvisits WHERE moz_places.id = moz_historyvisits.place_id;')"
+			CONFIG=$FIREFOX_DIR$(grep -i path $FIREFOX_PROFILE | awk -F= '{print $2}')
+			cd $CONFIG
+			DB=$CONFIG'/places.sqlite'
+			echo "$(sqlite3 $DB 'SELECT datetime(moz_historyvisits.visit_date/1000000,"unixepoch"), moz_places.url FROM moz_places, moz_historyvisits WHERE moz_places.id = moz_historyvisits.place_id ORDER by visit_date;')"
 		fi
 	done
 
