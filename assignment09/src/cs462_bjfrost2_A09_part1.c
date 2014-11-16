ceb69036c30420b49783508b6a387b2c1bf19c8c
Bucky Frost <frozenouthere@gmail.com>
2014-11-16 14:11:50 -0900

    Modified cs462_bjforst2_A09_part1.c
    Added a string doubler function. Will return a string with each
    character doubled.

--- assignment09/src/cs462_bjfrost2_A09_part1.c
+++ assignment09/src/cs462_bjfrost2_A09_part1.c
@@ -10,6 +10,7 @@
 #include <string.h>
 
 char* strflip(char *str);
+char* strdoubler(char *str);
 
 int main(int argc, char **argv)
 {
@@ -18,7 +19,40 @@ int main(int argc, char **argv)
 		  printf("%s\n",flipped_arg);
 		free (flipped_arg);
 	}
+
+	if (argc == 2) {
+		char *doubled_arg = strdoubler(argv[1]);
+		printf("%s\n",doubled_arg);
+		free (doubled_arg);
+	}
 	exit(0);
+}
+
+/**
+ * function strdoubler
+ *
+ * Takes a pointer to a c string.
+ *
+ * Returnas a pointer to memory allocated for a string that has repeated
+ * the characters of a string.
+ *
+ * Example: abc => aabbcc
+ */
+char* strdoubler(char *str)
+{
+	int string_size = strlen(str);
+
+	char *doubled_string = (char *)malloc(sizeof(char)*string_size);
+
+	int doubler_size = string_size*2 - 1;
+	int str_index = 0;
+
+	for (int ii = 0; ii < doubler_size; ++ii) {
+		doubled_string[ii++] = str[str_index];
+		doubled_string[ii] = str[str_index++];
+	}
+
+	return doubled_string;
 }
 
 /**
