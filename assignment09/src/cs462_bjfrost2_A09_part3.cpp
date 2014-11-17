292e8fc5072d1b4cad79a517cf895593d1873607
Bucky Frost <frozenouthere@gmail.com>
2014-11-16 19:24:24 -0900

    Adding files for completing part 3 of assignment 9.
    The source for the crackme and the socket library of Dr. Lawlor.

---
+++ assignment09/src/cs462_bjfrost2_A09_part3.cpp
@@ -0,0 +1,17 @@
+/**
+ * Bucky Frost
+ * CS 462
+ * File: cs462_bjfrost2_A09_part2.c
+ * Purpose: A crackme program that is approximately 100 lines of code and 2-3 operations
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "socket.h"
+
+
+int main(int argc, char* argv[])
+{
+	exit(0);
+}
